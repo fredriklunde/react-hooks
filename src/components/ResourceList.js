9da58ffa080b9107776b55224a8e69771e14aaa2
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-28 11:15:09 +0100

    component did update

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -10,6 +10,14 @@ class ResourceList extends React.Component {
         this.setState({resource: response.data});
     }
 
+    async componentDidUpdate(prevProps) {
+        if(prevProps.resource !== this.props.resource){
+            const response =  await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        
+            this.setState({resource: response.data});
+        }
+    }
+
     render() {
         return <div>{this.state.resource.length}</div>
     }
