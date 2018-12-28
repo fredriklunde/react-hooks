121af7a2c955f51fbf6fb5dd036e91169bd385ec
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-28 13:15:08 +0100

    fix

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,25 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-    state = { resource: [] };
+const ResourceList = ({ resource }) => {
+    const [resources, setResources ] = useState([]);
 
-    async componentDidMount(){
-        const response =  await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
         
-        this.setState({resource: response.data});
+        setResources(response.data);
     }
 
-    async componentDidUpdate(prevProps) {
-        if(prevProps.resource !== this.props.resource){
-            const response =  await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-        
-            this.setState({resource: response.data});
-        }
-    }
+    useEffect(() => {
+        fetchResource(resource);
+    }, []);
 
-    render() {
-        return <div>{this.state.resource.length}</div>
-    }
+    return <div>{resources.length}</div>
+    
 }
+
 export default ResourceList
